18718993943177240cff57a1d053c5a3047960ef
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2019-06-04 22:06:37 +0200

    updated background colors

--- scripts/bauhaus.js
+++ scripts/bauhaus.js
@@ -1,3 +1,17 @@
-var Bauhaus = {
-    
-};
+var bg = null;
+function onScrollEvent() {
+    var scrollTop = document.getElementsByTagName("html")[0].scrollTop || document.getElementsByTagName("body")[0].scrollTop;
+    bg.style.top = Math.floor(scrollTop*-0.27)+"px";
+}
+
+window.addEventListener("load", function() {
+    onScrollEvent();
+});
+
+function initEffects() {
+    bg = document.getElementById("bauhaus-background");
+    document.onscroll = onScrollEvent;
+    onScrollEvent();
+}
+
+document.addEventListener("DOMContentLoaded", initEffects);
