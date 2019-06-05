38d14feb13c6ad6d24bb609452683d1cfc32cb3a
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2019-06-05 22:22:54 +0200

    rewrote cards

--- scripts/bauhaus.js
+++ scripts/bauhaus.js
@@ -1,7 +1,10 @@
-var bg = null;
+var bg1 = null;
+var bg2 = null;
 function onScrollEvent() {
     var scrollTop = document.getElementsByTagName("html")[0].scrollTop || document.getElementsByTagName("body")[0].scrollTop;
-    bg.style.top = Math.floor(scrollTop*-0.27)+"px";
+    console.log(scrollTop);
+    bg1.style.top = Math.floor(scrollTop*-0.27-48)+"px";
+    bg2.style.top = Math.floor(scrollTop*-0.15-48)+"px";
 }
 
 window.addEventListener("load", function() {
@@ -9,7 +12,8 @@ window.addEventListener("load", function() {
 });
 
 function initEffects() {
-    bg = document.getElementById("bauhaus-background");
+    bg1 = document.getElementById("bg1");
+    bg2 = document.getElementById("bg2");
     document.onscroll = onScrollEvent;
     onScrollEvent();
 }
